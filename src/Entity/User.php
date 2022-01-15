ef4b3b071c7cf88df44390efb4052b326f76d1c3
Boutin Raphael <raphael.boutin@etu.u-bordeaux.fr>
2022-01-15 18:02:21 +0100

    fin css tricks

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -299,5 +299,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
         return false;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
     
 }
