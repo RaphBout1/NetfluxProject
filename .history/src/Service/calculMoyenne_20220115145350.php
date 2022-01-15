d1db2fab7b0825b4a5878ae4a4a508459ce65b55
agathe Domenger <prenom.nom@etu.u-bordeaux.fr>
2022-01-15 15:26:53 +0100

    petite sauvegarde

---
+++ .history/src/Service/calculMoyenne_20220115145350.php
@@ -0,0 +1,27 @@
+### permet d'initialiser les moyennes
+        $series = $entityManager
+            ->getRepository(Series::class)
+            ->findAll();
+        
+
+        foreach($series as $serie){
+            $ratings = $entityManager
+            ->getRepository(Rating::class)
+            ->findBy(
+                ['series' => $serie],
+            );
+            $moyenne = 0;
+            $nbRating = 0;
+            foreach($ratings as $rat){
+                 $moyenne = $moyenne + $rat->getValue();
+                 $nbRating ++;
+
+            }
+            if($nbRating!=0){
+                $moyenne = $moyenne/$nbRating;
+                $moyenne = number_format($moyenne,1);
+                $serie->setnoteUser($moyenne);
+            }
+            
+
+        }   
---
+++ .history/src/Service/calculMoyenne_20220115145801.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Service;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class calculMoyenne
+{
+
+    use App\Entity\Episode;
+use App\Entity\User;
+use App\Entity\Series;
+use App\Entity\Season;
+
+    public function __construct()
+    {
+       
+    }
+
+    public function setMoyenne(EntityManager $entityManager)
+    {
+        $series = $entityManager
+        ->getRepository(Series::class)
+        ->findAll();
+    
+
+    foreach($series as $serie){
+        $ratings = $entityManager
+        ->getRepository(Rating::class)
+        ->findBy(
+            ['series' => $serie],
+        );
+        $moyenne = 0;
+        $nbRating = 0;
+        foreach($ratings as $rat){
+             $moyenne = $moyenne + $rat->getValue();
+             $nbRating ++;
+
+        }
+        if($nbRating!=0){
+            $moyenne = $moyenne/$nbRating;
+            $moyenne = number_format($moyenne,1);
+            $serie->setnoteUser($moyenne);
+        }
+        
+
+    }   
+    }
+
+
+}
+
+
+### permet d'initialiser les moyennes
+        
